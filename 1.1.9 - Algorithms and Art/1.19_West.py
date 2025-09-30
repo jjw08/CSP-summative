0d1bdb37aa0694f71b1e20fa0c9e51ff826ddf4b
jjw08 <310289@shakopeeschools.org>
2025-09-30 10:14:25 -0500

    Update 1.19_West.py

--- 1.1.9 - Algorithms and Art/1.19_West.py
+++ 1.1.9 - Algorithms and Art/1.19_West.py
@@ -3,12 +3,6 @@ painter = trtl.Turtle()
 #Add custom plane turtle
 trtl.addshape("plane", ((-64,-8),(-40,8),(32,8),(48,32),(48,8),(40,-8)))
 
-#Create lists
-plane_color = [trtl.textinput("Plane color", 'What color do you want the plane to be?')]
-plane_shape = ["plane"]
-plane_full = []
-plane_speed = int((trtl.textinput("Plane Speed", 'How fast should the plane be? (1-10)')))
-
 
 #Draw runway, add backround
 trtl.bgcolor("CadetBlue1")
@@ -30,36 +24,56 @@ trtl.update()
 trtl.tracer(1)
 
 
+#create system loop
+
+
+loopy = 'y'
+while loopy == 'y':
+    
+
+#Create lists
+    plane_color = [trtl.textinput("Plane color", 'What color do you want the plane to be?')]
+    plane_shape = ["plane"]
+    plane_full = []
+    plane_speed = int((trtl.textinput("Plane Speed", 'How fast should the plane be? (1-10)')))
+
+
+
+
+
 
 # create plane
-for s in plane_shape:
-  t = trtl.Turtle(shape=s)
-  c = plane_color.pop()
-  t.color(c)
-  t.penup()
+    for s in plane_shape:
+        t = trtl.Turtle(shape=s)
+        c = plane_color.pop()
+        t.color(c)
+        t.penup()
   
-  plane_full.append(t)
+    plane_full.append(t)
 
-startx = 300
-starty = 8
+    startx = 300
+    starty = 8
 
-for t in plane_full:
-   t.goto(startx,starty)
-   t.setheading(90)
+    for t in plane_full:
+        t.goto(startx,starty)
+        t.setheading(90)
 
 #trtl.update()
 
 
 #Make plane movement
-t.speed(plane_speed)
-t.goto(-200,8)
-t.goto(-500,150)
+    t.speed(plane_speed)
+    t.goto(-200,8)
+    t.goto(-500,150)
 
 
 #Ask user if they want to run the program again
+    answer = trtl.textinput("Run Again?", 'Do you want to run the program again? (y/n)')
+    if answer == 'y':
+        loopy == 'y'
+        
+    else:
+        loopy == 'n'
+        wn = trtl.Screen()
+        wn.mainloop()     
 
-
-
-
-wn = trtl.Screen()
-wn.mainloop()

7268e17613e80c85d901a9e119b753c33d0f82e6
jjw08 <310289@shakopeeschools.org>
2025-10-27 10:45:22 -0500

    Update 125_JettWest.py

---
+++ 1.2.5 - Shall We Play a Game/125_JettWest.py
@@ -0,0 +1,12 @@
+# Code here
+import turtle as trtl
+import random as rand
+
+#----- Variables
+soccer_ball = "soccerball.gif"
+goal = "goal.gif"
+
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+wn.addshape(soccer_ball)
+

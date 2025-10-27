ee820340c4be195c0ed65d9527834bab70552309
jjw08 <310289@shakopeeschools.org>
2025-10-27 11:15:26 -0500

    Update 125_JettWest.py

--- 1.2.5 - Shall We Play a Game/125_JettWest.py
+++ 1.2.5 - Shall We Play a Game/125_JettWest.py
@@ -2,11 +2,30 @@
 import turtle as trtl
 import random as rand
 
-#----- Variables
+#----- Variables and screen setup
 soccer_ball = "soccerball.gif"
 goal = "goal.gif"
+
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(soccer_ball)
+wn.bgpic(goal)
+trtl.Screen().bgcolor("#00bfff")
 
+ball = trtl.Turtle()
+ball.shape(soccer_ball)
+ball.turtlesize(2)
+ball.penup()
+ball.goto(0,-120)
+wn.tracer(False)
+
+#----functions
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()

f0ff23473f4363113c1c5f4bf85a50a71820e0f3
jjw08 <310289@shakopeeschools.org>
2025-10-28 11:15:17 -0500

    Update 125_JettWest.py

--- 1.2.5 - Shall We Play a Game/125_JettWest.py
+++ 1.2.5 - Shall We Play a Game/125_JettWest.py
@@ -1,11 +1,10 @@
-# Code here
 import turtle as trtl
 import random as rand
 
 #----- Variables and screen setup
 soccer_ball = "soccerball.gif"
 goal = "goal.gif"
-
+goaler = "goalie.gif"
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
@@ -13,15 +12,32 @@ wn.addshape(soccer_ball)
 wn.bgpic(goal)
 trtl.Screen().bgcolor("#00bfff")
 
+
+#Create ball 
 ball = trtl.Turtle()
 ball.shape(soccer_ball)
-ball.turtlesize(2)
 ball.penup()
 ball.goto(0,-120)
 wn.tracer(False)
 
-#----functions
+#Add goalie
 
+goalie = trtl.Turtle()
+goalie.shape(goaler)
+goalie.penup()
+goalie.goto(0,0)
+wn.tracer(False)
+
+
+#----functions
+#Create onclick functions for a, w, d, move ball to specific positions
+
+#create specific random positions for the golie to move to
+
+#if ball and goalie = same x.cor then print you lose, end game
+
+
+#if ball and goalie = diff x.cor then add 1 score, reloop the game.
 
 
 

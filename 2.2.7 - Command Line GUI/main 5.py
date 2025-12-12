60cfba1eb4c47b0bfa73716449379ed5f692302f
jjw08 <310289@shakopeeschools.org>
2025-12-12 09:40:35 -0600

    Create main 5.py

---
+++ 2.2.7 - Command Line GUI/main 5.py
@@ -0,0 +1,80 @@
+# p227_starter_one_button_shell.py
+# Note this will not run in the code editor and must be downloaded
+
+import subprocess
+import tkinter as tk
+import tkinter.scrolledtext as tksc
+from tkinter import filedialog
+from tkinter.filedialog import asksaveasfilename
+
+def do_command(command):
+    global command_textbox, url_entry
+
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
+    
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+
+    p = subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    cmd_results, cmd_errors = p.communicate()
+    command_textbox.insert(tk.END, cmd_results)
+    command_textbox.insert(tk.END, cmd_errors)
+
+# Save function.
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+# set up button to run the do_command function
+ping_btn = tk.Button(frame, text="Ping URL", command=lambda:do_command("ping -c 10"))
+ping_btn.pack()
+
+NS_btn = tk.Button(frame, text="NSlookup", command=lambda:do_command("nslookup"))
+NS_btn.pack()
+
+ifconfig_btn = tk.Button(frame, text="Trace route", command=lambda:do_command("dig +trace"))
+ifconfig_btn.pack()
+
+save_btn = tk.Button(frame, text="Save as text", command=lambda:mSave())
+save_btn.pack()
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
+
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()
+
+root.mainloop()
